9737edbf73f79dde8f6aaec62125496182a9ce40
Yonghao Jin <lucasyonghao@outlook.com>
2024-03-04 15:51:23 -0800

    llpivot done

--- llrec.cpp
+++ llrec.cpp
@@ -4,4 +4,23 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
-
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+    if(head == nullptr){
+        smaller = nullptr;
+        larger = nullptr;
+        return;
+    }
+    
+    if(head->val > pivot){
+        llpivot(head->next, smaller, larger, pivot);
+        head->next = larger;
+        larger = head;
+    }
+    else{
+        llpivot(head->next, smaller, larger, pivot);
+        head->next = smaller;
+        smaller = head;
+    }
+    head = nullptr;
+    return;
+}
